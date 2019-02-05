f3e94500896181ed4c4570b95bb4cbf1942ae4c9
mat2567 <matthew.l.bollinger@gmail.com>
2019-02-05 14:17:39 -0600

    Update create.php

--- api/v2/images/create.php
+++ api/v2/images/create.php
@@ -18,3 +18,82 @@ $db = $database->getConnection();
 $image = new Image($db);
 
 $data = json_decode(file_get_contents("php://input"));
+// make sure data is not empty
+if(
+    !empty($data->name) &&
+    !empty($data->description) &&
+    !empty($data->createdBy) &&
+    !empty($data->configJson)
+){
+ 
+    // set product property values
+    $product->name = $data->name;
+    $product->description = $data->description;
+    $product->createdBy = $data->createdBy;
+    $product->configJson = $data->configJson;
+    $product->created = date('Y-m-d H:i:s');
+ 
+    // create the product
+    if($product->create()){
+ 
+        // set response code - 201 created
+        http_response_code(201);
+ 
+        // tell the user
+        echo json_encode(array("message" => "Product was created."));
+    }
+ 
+    // if unable to create the product, tell the user
+    else{
+ 
+        // set response code - 503 service unavailable
+        http_response_code(503);
+ 
+        // tell the user
+        echo json_encode(array("message" => "Unable to create product."));
+    }
+}
+ 
+// tell the user data is incomplete
+else{
+ 
+    // set response code - 400 bad request
+    http_response_code(400);
+ 
+    // tell the user
+    echo json_encode(array("message" => "Unable to create product. Data is incomplete."));
+}
+
+
+// create product
+function create(){
+ 
+    // query to insert record
+    $query = "INSERT INTO images SET name=:name, name=:name, description=:description, configJson=:configJson, created=:created";
+ 
+    // prepare query
+    $stmt = $this->conn->prepare($query);
+ 
+    // sanitize
+    $this->name=htmlspecialchars(strip_tags($this->name));
+    $this->price=htmlspecialchars(strip_tags($this->description));
+    $this->description=htmlspecialchars(strip_tags($this->description));
+    $this->category_id=htmlspecialchars(strip_tags($this->category_id));
+    $this->created=htmlspecialchars(strip_tags($this->created));
+ 
+    // bind values
+    $stmt->bindParam(":name", $this->name);
+    $stmt->bindParam(":description", $this->description);
+    $stmt->bindParam(":configJson", $this->configJson);
+    $stmt->bindParam(":createdBy", $this->createdBy);
+    $stmt->bindParam(":created", $this->created);
+ 
+    // execute query
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+     
+}
+?>
