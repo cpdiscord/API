519739b65f6692184986522fa3e290411faaba15
mat2567 <matthew.l.bollinger@gmail.com>
2019-02-05 14:21:56 -0600

    Fixed a bug where it could create -- Create was in wrong file

--- api/v2/objects/image.php
+++ api/v2/objects/image.php
@@ -31,6 +31,36 @@
 
       return $stmt;
     }
+    // create image
+function create(){
+ 
+    // query to insert record
+    $query = "INSERT INTO images SET name=:name, description=:description, configJson=:configJson, created=:created";
+ 
+    // prepare query
+    $stmt = $this->conn->prepare($query);
+ 
+    // sanitize
+    $this->name=htmlspecialchars(strip_tags($this->name));
+    $this->price=htmlspecialchars(strip_tags($this->description));
+    $this->description=htmlspecialchars(strip_tags($this->description));
+    $this->category_id=htmlspecialchars(strip_tags($this->category_id));
+    $this->created=htmlspecialchars(strip_tags($this->created));
+ 
+    // bind values
+    $stmt->bindParam(":name", $this->name);
+    $stmt->bindParam(":description", $this->description);
+    $stmt->bindParam(":configJson", $this->configJson);
+    $stmt->bindParam(":createdBy", $this->createdBy);
+    $stmt->bindParam(":created", $this->created);
+ 
+    // execute query
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+     
+}
     // no products found will be here
-
 }
